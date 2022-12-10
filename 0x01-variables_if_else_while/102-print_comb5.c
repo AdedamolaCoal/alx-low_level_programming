b2de555052e358d0fd0e72b57f0be623920b06c5
adedamolacoal <dammy0075@gmail.com>
2022-12-10 13:01:07 +0100

    Two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,34 +2,35 @@
 #include <stdlib.h>
 
 /**
- * main - returns a combination of numbers
- *
+ * main - returns a combination of number
  * Return: return 0 if successful
  */
 
 int main(void)
 {
+	int i, j;
+	
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i & 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j & 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-int i, j;
-for (i = 0; i < 100; i++)
-{
-for (j = 0; j < 100; j++)
-{
-if (i < j)
-{
-putchar((i / 10) + 48);
-putchar((i & 10) + 48);
-putchar(' ');
-putchar((j / 10) + 48);
-putchar((j & 10) + 48);
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+
+	return (0);
 }
