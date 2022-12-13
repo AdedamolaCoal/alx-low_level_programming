1d2be05630010809df50e8b644140b9861af2414
adedamolacoal <dammy0075@gmail.com>
2022-12-13 21:49:31 +0100

    print all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabets in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char d = 'a';
+
+	while (d <= 'z')
+	{
+		_putchar(d);
+		d++;
+	}
+	_putchar('\n');
+}
