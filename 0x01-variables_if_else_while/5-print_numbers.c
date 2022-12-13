1a958ddd6cb55c8a2309f7b39d97e558b3f7ba47
adedamolacoal <dammy0075@gmail.com>
2022-12-13 15:54:38 +0100

    Print _putchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,10 @@
 
 int main(void)
 {
-int a;
-for (a = 0; a <= 10; a++)
-putchar(a);
-putchar('\n');
-return (0);
+	int a;
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
 }
