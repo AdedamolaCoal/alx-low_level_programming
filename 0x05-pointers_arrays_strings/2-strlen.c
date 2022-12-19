9eeea3b048d7eca52a0ac4efd29f7dd09fdcdb74
adedamolacoal <dammy0075@gmail.com>
2022-12-19 22:12:36 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returs the length of a string.
+ * @str: string
+ * Return: Length.
+ */
+
+size_t_strlen(const char *str)
+{
+	size_t_len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is based on C strings, arrays and pointers.
