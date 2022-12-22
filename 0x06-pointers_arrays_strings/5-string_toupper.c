245dd28580e6747890b5fe06038873c1d2064a77
adedamolacoal <dammy0075@gmail.com>
2022-12-22 01:03:13 +0100

    Cap string and String toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,14 @@
 
 char *string_toupper(char *str)
 {
-	int index = 0;
+	int i;
 
-	while (str[index])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] == 32;
-		index++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
 	}
 
 	return (str);
