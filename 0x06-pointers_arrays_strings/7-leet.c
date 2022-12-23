a0e5138e5685300551b4a83cdc4dba2938afce59
adedamolacoal <dammy0075@gmail.com>
2022-12-23 02:34:12 +0100

    hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 
 char *leet(char *x)
 {
-	int a = 0, b = 0, l = 5;
+	int a = 0, b, l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
@@ -20,7 +20,7 @@ char *leet(char *x)
 		while (b < l)
 		{
 			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-				x[a] = tre[b];
+				x[a] = trw[b];
 			b++;
 		}
 		a++;
