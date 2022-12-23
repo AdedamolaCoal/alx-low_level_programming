7bae8a3b0751ad24d5b4bae1f8a27f1b840807ab
adedamolacoal <dammy0075@gmail.com>
2022-12-23 01:16:04 +0100

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,40 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
+ * cap_string - Capitalizes all words of a string
  * @s: input string.
- * Return: The pointer to dest.
+ *
+ * Return: A pointer to the changed string.
  */
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int separators[] = (32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125);
+	int count = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (s[index])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == separators[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+		
+		if (s[index - 1] == ' ' ||
+				s[index - 1] == '\t' ||
+				s[index - 1] == '\n' ||
+				s[index - 1] == ',' ||
+				s[index - 1] == ';' ||
+				s[index - 1] == '.' ||
+				s[index - 1] == '!' ||
+				s[index - 1] == '?' ||
+				s[index - 1] == '"' ||
+				s[index - 1] == '(' ||
+				s[index - 1] == ')' ||
+				s[index - 1] == '{' ||
+				s[index - 1] == '}' ||
+				index == 0)
+					s[index] -= 32;
+		
+		index++;
 	}
+
 	return (s);
 }
 
