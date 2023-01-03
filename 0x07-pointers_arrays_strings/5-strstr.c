8af4f4c1ab401eafe44f06e09d5319c2cfea4e05
adedamolacoal <dammy0075@gmail.com>
2023-01-03 04:47:43 +0100

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,18 +13,18 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != '\0')
 	{
-		str1 = haystack; /*values*/
-		str2 = needle;
+		*str1 = haystack; /*values*/
+		*str2 = needle;
 
 		/*Star WHILE*/
 		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
 			haystack++;
-			str2++;
+			*str2++;
 		}
 		if (*str2 == '\0')
-			return (str1);
-		haystack = str1 + 1;
+			return (*str1);
+		haystack = *str1 + 1;
 	}
 	return (0);
 }
