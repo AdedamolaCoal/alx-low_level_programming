782ca01981b1fd068b4e63d85f880524e71e7cb1
adedamolacoal <dammy0075@gmail.com>
2023-01-13 00:30:19 +0100

    Update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,22 +9,23 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+unsigned long mul;
+int i, j;
 
-	if (argc != 1)
+	if (argc != 3)
 	{ printf("Error\n");
-		exit(98); }
+	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{ printf("Error\n");
-				exit(98); }
+			exit(98); }
 		}
+
 	}
-	mul = atol(argv[i]) * atol(argv[i]);
+	mul = atol(argv[2]) * atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
